978d682d14ba16e5ceecbddda0f2cc030496d1f8
overflow-gitch <tarikbikhandafne@outlook.com>
2020-12-05 13:05:03 -0400

    Added an enemy template, bubble class and added some annotation.

---
+++ Enemies.js
@@ -0,0 +1,69 @@
+//gets the canvas element and it's info for drawing.
+const canvas = document.getElementsByTagName('canvas')[0];
+const context = canvas.getContext('2d');
+
+
+//This creates circles that can be targeted and modified
+class Bubble {
+    constructor(){
+        this.radius = 20;
+        this.x = this.radius + (Math.random() * (canvas.width - (this.radius * 2)));
+		this.y = this.radius + (Math.random() * (canvas.height - (this.radius * 2)));
+        this.velocity = {
+			x: 1,
+			y: 1
+        }
+        this.color = context.fillStyle = "white";
+    }
+    draw(){
+        context.beginPath();
+        context.arc(this.x,this.y,this.radius,0, 2 * Math.PI);
+        context.closePath();
+        context.stroke();
+        context.fillStyle = this.color;
+        context.fill();
+    }
+    update(){
+        this.checkCanvasBounds();
+
+        for (let bubble of bubbles) {
+            //bubble.checkCanvasBounds();
+			if (this.isColliding(bubble)) {
+                this.velocity.x = -this.velocity.x;
+                this.velocity.y = -this.velocity.y;
+                this.color = 'rgb(' + Math.floor(Math.random() * 255) + ', ' + Math.floor(Math.random() * 255) + ', ' + Math.floor(Math.random() * 255) + ')'             
+            }
+		}
+        this.x += this.velocity.x;
+        this.y += this.velocity.y;
+        this.draw();
+    }
+    //keeps the bubble inside the canvas
+    checkCanvasBounds() {
+		if ((this.x + this.radius) > canvas.width || (this.x - this.radius) < 0) {
+            this.velocity.x = -this.velocity.x;
+		}
+		if ((this.y + this.radius) > canvas.height || (this.y - this.radius) < 0) {
+			this.velocity.y = -this.velocity.y;
+        }
+    }
+    isColliding(bubble) {
+        //The logic behind this code was taken from last semester's UI project. This is because finding the distance between two points means to do the math as done in math.hypot method.
+		let distance = Math.hypot(this.x - bubble.x, this.y - bubble.y);
+		return (distance <= (this.radius + bubble.radius) && distance > 0);
+	}
+}
+let bubbles = [];
+let bubbleNum = 3;
+for (let index = 0; index < bubbleNum; index++) {
+    bubbles.push(new Bubble())
+    
+}
+console.log(bubbles);
+function animate() {
+    requestAnimationFrame(animate);
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    for (item of bubbles)
+        item.update();
+   }
+animate();
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # WebProgrammingProject
 A game made with html canvas
+
+Object of game: you are a small bubble at the bottom of the screen. Get to the top without touching any of the other bubbles.
+
+Enemy behaviour: Each game starts with lots of big bubbles that change color and become smaller as they bump into things. As they become smaller, they become faster, after becoming a certain size, they dissapear.
+
+General game flow: The player is expected to get to the end without touching any of the enemy bubbles. If the enemies are in contact with the player, the player loses a life. The player is either expected to move around the bubbles, or wait for them to dissapear.
+
+Controls: The player bubble will move with the mouse. 
+
+Notes: I expect to be using lots of code from the previous assignment.
