da8d185ab42da5d5c5efcb221ad7555467157319
overflow-gitch <tarikbikhandafne@outlook.com>
2020-12-10 17:14:33 -0400

    Added replay functionality.
    When the game if finished, you can play again. Updated the readme and made the elements align as columns.

--- project/Main.js
+++ project/Main.js
@@ -1,26 +1,43 @@
 const canvas = document.getElementsByTagName('canvas')[0];
 const context = canvas.getContext('2d');
+let saveState = canvas;
+const gameOverText = document.createElement("h2");
+const replayButton = document.createElement("button");
+replayButton.style.display = "block";
+replayButton.innerHTML = "Replay";
+replayButton.addEventListener("click", function () {
+    document.body.replaceChild(canvas, gameOverText);
+    document.body.removeChild(replayButton);
+    newGame();
+});
 
+function newGame(){
+    for (let index = 0; index < bubbleNum; index++) {
+        bubbles.pop(bubbles[index]);
+    }
+    for (let index = 0; index < bubbleNum; index++) {
+        bubbles.push(new Bubble());
+    }
+    player = new PlayerBubble(250,475);
+    animate();
+}
 let bubbles = [];
 let bubbleNum = 3;
 for (let index = 0; index < bubbleNum; index++) {
     bubbles.push(new Bubble());
-    
 }
 console.log(bubbles);
 let player = new PlayerBubble(250,475);
 
 function gameOver(win){
-    let gameOverText = document.createElement("h2");
-
     if (win){
         gameOverText.innerHTML = "huh, you won."
     }
     else
         gameOverText.innerHTML = "game is over. go home."
     cancelAnimationFrame(request);
-    //document.body.removeChild(canvas);
     document.body.replaceChild(gameOverText, canvas);
+    document.body.appendChild(replayButton);
 }
 let request;
 function animate() {
